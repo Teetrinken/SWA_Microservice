9d8b87a172a447ff41604b7f7106b2094c7f9bff
Sebastian <Sebastian.Saliger@gmail.com>
2018-05-28 13:57:07 +0200

    Script Anpassungen

--- Vagrantfile
+++ Vagrantfile
@@ -68,7 +68,6 @@ Vagrant.configure("2") do |config|
   #   apt-get update
   #   apt-get install -y apache2
   # SHELL
-  config.vm.provision "docker"
-  config.vm.provision :shell, path: "scripts/bootstrap.sh"
+  config.vm.provision :shell, path: "scripts/bootstrap.sh", privileged: false
 
 end
--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,5 +1,4 @@
-! /bin/sh
-sudo apt-get install sshpass
+#!/bin/sh
 
 export SSHPASS=vagrant
 sshpass -e sftp -oBatchMode=no -b - vagrant@192.168.50.103 << !
@@ -20,6 +19,4 @@ docker network create -d bridge bridge-network
 
 docker run --detach --name=parcelsize --network=bridge-network -p 8000:8000 teetrinken/parcelsize:8.0
 docker run --detach --name=parcel_webserver --network=bridge-network -p 8080:8080 teetrinken/parcelwebserver:asset-1.0
-docker run --detach --name=mysql_parcelsize --mount type=bind,src=/vagrant/scripts,dst=/docker-entrypoint-initdb.d/ \
---network=bridge-network -p 3306:3306 --env="MYSQL_ROOT_PASSWORD=mypass" mysql:5.7
-
+docker run --detach --name=mysql_parcelsize --mount type=bind,src=/vagrant/scripts/mysql,dst=/docker-entrypoint-initdb.d/ --network=bridge-network -p 3306:3306 --env="MYSQL_ROOT_PASSWORD=mypass" mysql:5.7
