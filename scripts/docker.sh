0511495c5048b299faaeb37f256afe6903b1b737
Sebastian <Sebastian.Saliger@gmail.com>
2018-05-30 23:12:07 +0200

    Docker-Script angepasst

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 vagrant ssh <<EOT
-docker load < images/mysql.tar
-docker load < images/parcelsize.tar
-docker load < images/parcelwebserver.tar
+sudo docker load < /vagrant/images/mysql.tar
+sudo docker load < /vagrant/images/parcelsize.tar
+sudo docker load < /vagrant/images/parcelwebserver.tar
 
-docker network create -d bridge bridge-network
+sudo docker network create -d bridge bridge-network
 
-docker run --detach --name=parcelsize --network=bridge-network -p 8000:8000 teetrinken/parcelsize:8.0
-docker run --detach --name=parcel_webserver --network=bridge-network -p 8080:8080 teetrinken/parcelwebserver:asset-1.0
-docker run --detach --name=mysql_parcelsize --mount type=bind,src=/vagrant/scripts/mysql,dst=/docker-entrypoint-initdb.d/ --network=bridge-network -p 3306:3306 --env="MYSQL_ROOT_PASSWORD=mypass" mysql:5.7
+sudo docker run --detach --name=parcelsize --network=bridge-network -p 8000:8000 teetrinken/parcelsize:8.0
+sudo docker run --detach --name=parcel_webserver --network=bridge-network -p 8080:8080 teetrinken/parcelwebserver:asset-1.0
+sudo docker run --detach --name=mysql_parcelsize --mount type=bind,src=/vagrant/scripts/mysql,dst=/docker-entrypoint-initdb.d/ --network=bridge-network -p 3306:3306 --env="MYSQL_ROOT_PASSWORD=mypass" mysql:5.7
 exit
 EOT
